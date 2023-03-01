dc8f01c23c66c5826268e8d197d54fed47fd359e
Tirso Oliveira <tirso.oliveira@gmail.com>
2023-03-01 12:16:45 +0100

    modified:   app.py 	new file:   favicon.ico 	modified:   requirements.txt 	new file:   static/custom.css 	new file:   static/favicon.ico 	new file:   templates/index.html 	new file:   templates/layout.html

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template, request, flash, Markup, redirect, url_for
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms.validators import DataRequired, Length, Regexp
+from wtforms.fields import *
+from flask_bootstrap import Bootstrap5, SwitchField
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.secret_key = 'dev'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
+# set default button sytle and size, will be overwritten by macro parameters
+app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
+app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
+
+# set default icon title of table actions
+app.config['BOOTSTRAP_TABLE_VIEW_TITLE'] = 'Read'
+app.config['BOOTSTRAP_TABLE_EDIT_TITLE'] = 'Update'
+app.config['BOOTSTRAP_TABLE_DELETE_TITLE'] = 'Remove'
+app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
+
+bootstrap = Bootstrap5(app)
+db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template('index.html', utc_dt="2023-03-01")
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
